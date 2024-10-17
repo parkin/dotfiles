ce3582b0dada7afdc821efbe58be06d5583b4e31
Will Parkin <wmparkin@gmail.com>
2024-10-17 03:12:39 +0000

    implemented iron.nvim with a fork for code block running

--- modules/features/neovim/nvim/lua/plugins/code-runner.lua
+++ modules/features/neovim/nvim/lua/plugins/code-runner.lua
@@ -1,6 +1,8 @@
 return {
-  "hkupty/iron.nvim",
-  config = function(plugins, opts)
+  -- "hkupty/iron.nvim", -- This is the original
+  "frere-jacques/iron.nvim",
+  branch = "feature_send_code_block", -- This is a fork that added code block running
+  init = function()
     local iron = require("iron.core")
 
     iron.setup({
@@ -9,31 +11,42 @@ return {
         scratch_repl = true,
         -- Your repl definitions come here
         repl_definition = {
-          python = {
+          sh = {
             -- Can be a table or a function that
             -- returns a table (see below)
-            command = { "python" },
+            command = { "zsh" },
+          },
+          python = {
+            -- command = { "python3" }, -- or { "ipython", "--no-autoindent" }
+            command = { "jupyter-console" },
+            format = require("iron.fts.common").bracketed_paste_python,
+            block_deviders = { "# %%", "#%%" },
           },
         },
         -- How the repl window will be displayed
         -- See below for more information
-        repl_open_cmd = require("iron.view").right(60),
+        -- repl_open_cmd = require("iron.view").bottom(40),
+        repl_open_cmd = "vertical botright 80 split",
       },
       -- Iron doesn't set keymaps by default anymore.
       -- You can set them here or manually add keymaps to the functions in iron.core
       keymaps = {
-        send_motion = "<space>rc",
-        visual_send = "<space>rc",
-        send_file = "<space>rf",
-        send_line = "<space>rl",
-        send_mark = "<space>rm",
-        mark_motion = "<space>rmc",
-        mark_visual = "<space>rmc",
-        remove_mark = "<space>rmd",
-        cr = "<space>r<cr>",
-        interrupt = "<space>r<space>",
-        exit = "<space>rq",
-        clear = "<space>rx",
+        send_motion = "<space>ic",
+        visual_send = "<space>ic",
+        send_file = "<space>if",
+        send_line = "<space>il",
+        send_paragraph = "<space>ip",
+        send_until_cursor = "<space>iu",
+        send_mark = "<space>im",
+        send_code_block = "<space>ib",
+        send_code_block_and_move = "<space>in",
+        mark_motion = "<space>imc",
+        mark_visual = "<space>imc",
+        remove_mark = "<space>imd",
+        cr = "<space>i<cr>",
+        interrupt = "<space>i<space>",
+        exit = "<space>iq",
+        clear = "<space>icl",
       },
       -- If the highlight is on, you can change how it looks
       -- For the available options, check nvim_set_hl
@@ -46,7 +59,7 @@ return {
     -- iron also has a list of commands, see :h iron-commands for all available commands
     vim.keymap.set("n", "<space>rs", "<cmd>IronRepl<cr>")
     vim.keymap.set("n", "<space>rr", "<cmd>IronRestart<cr>")
-    vim.keymap.set("n", "<space>rF", "<cmd>IronFocus<cr>")
+    vim.keymap.set("n", "<space>rf", "<cmd>IronFocus<cr>")
     vim.keymap.set("n", "<space>rh", "<cmd>IronHide<cr>")
   end,
 }
