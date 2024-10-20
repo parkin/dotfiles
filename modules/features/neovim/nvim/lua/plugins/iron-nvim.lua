7bf592fbb6ed68be3f988df4cee9590fa60b441a
Will Parkin <wmparkin@gmail.com>
2024-10-19 23:35:39 -0400

    use <leader>i for the Iron commands as well

--- modules/features/neovim/nvim/lua/plugins/iron-nvim.lua
+++ modules/features/neovim/nvim/lua/plugins/iron-nvim.lua
@@ -31,22 +31,22 @@ return {
       -- Iron doesn't set keymaps by default anymore.
       -- You can set them here or manually add keymaps to the functions in iron.core
       keymaps = {
-        send_motion = "<space>ic",
-        visual_send = "<space>ic",
-        send_file = "<space>if",
-        send_line = "<space>il",
-        send_paragraph = "<space>ip",
-        send_until_cursor = "<space>iu",
-        send_mark = "<space>im",
-        send_code_block = "<space>ib",
-        send_code_block_and_move = "<space>in",
-        mark_motion = "<space>imc",
-        mark_visual = "<space>imc",
-        remove_mark = "<space>imd",
-        cr = "<space>i<cr>",
-        interrupt = "<space>i<space>",
-        exit = "<space>iq",
-        clear = "<space>icl",
+        send_motion = "<leader>ic",
+        visual_send = "<leader>ic",
+        send_file = "<leader>if",
+        send_line = "<leader>il",
+        send_paragraph = "<leader>ip",
+        send_until_cursor = "<leader>iu",
+        send_mark = "<leader>im",
+        send_code_block = "<leader>ib",
+        send_code_block_and_move = "<leader>in",
+        mark_motion = "<leader>imc",
+        mark_visual = "<leader>imc",
+        remove_mark = "<leader>imd",
+        cr = "<leader>i<cr>",
+        interrupt = "<leader>i<leader>",
+        exit = "<leader>iq",
+        clear = "<leader>icl",
       },
       -- If the highlight is on, you can change how it looks
       -- For the available options, check nvim_set_hl
@@ -57,9 +57,9 @@ return {
     })
 
     -- iron also has a list of commands, see :h iron-commands for all available commands
-    vim.keymap.set("n", "<space>rs", "<cmd>IronRepl<cr>")
-    vim.keymap.set("n", "<space>rr", "<cmd>IronRestart<cr>")
-    vim.keymap.set("n", "<space>rf", "<cmd>IronFocus<cr>")
-    vim.keymap.set("n", "<space>rh", "<cmd>IronHide<cr>")
+    vim.keymap.set("n", "<leader>ii", "<cmd>IronRepl<cr>")
+    vim.keymap.set("n", "<leader>ir", "<cmd>IronRestart<cr>")
+    vim.keymap.set("n", "<leader>if", "<cmd>IronFocus<cr>")
+    vim.keymap.set("n", "<leader>ih", "<cmd>IronHide<cr>")
   end,
 }
