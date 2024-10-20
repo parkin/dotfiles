d340fdc753780d873996884dd0d2ce93261e1ad6
Will Parkin <wmparkin@gmail.com>
2024-10-20 15:32:17 -0400

    added nh, yet another nix helper

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -109,6 +109,13 @@
     ];
   };
 
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
+    flake = "/home/parkin/.dotfiles";
+  };
+
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
