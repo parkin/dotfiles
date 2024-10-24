0e867fb46dadcb2b64e30b97bdc066e966d52966
Will Parkin <wmparkin@gmail.com>
2024-10-23 21:58:40 -0400

    use nixpkgs from my flake for nixd lsp for neovim

--- modules/features/neovim/nvim/lua/plugins/lsp.lua
+++ modules/features/neovim/nvim/lua/plugins/lsp.lua
@@ -2,6 +2,9 @@
 -- Note that since I'm using Nix, I have Mason disabled, so I need to have the LSP's added to my home-manager home.nix.
 -- 1. add the nix lsp package to ~/.config/home-manager/home.nix
 -- 2. add a line to this file
+--
+
+local dotfiles = "/home/parkin/.dotfiles"
 
 return {
   {
@@ -16,17 +19,20 @@ return {
           settings = {
             nixd = {
               nixpkgs = {
-                expr = "import <nixpkgs> { }",
+                -- expr = "import <nixpkgs> { }",
+                expr = 'import (builtins.getFlake "' .. dotfiles .. '").inputs.nixpkgs { }',
               },
               formatting = {
                 command = { "nixfmt" },
               },
               options = {
                 nixos = {
-                  expr = '(builtins.getFlake "/home/parkin/.dotfiles").nixosConfigurations."galacticboi-nixos".options',
+                  expr = '(builtins.getFlake "' .. dotfiles .. '").nixosConfigurations."galacticboi-nixos".options',
                 },
                 home_manager = {
-                  expr = '(builtins.getFlake  "/home/parkin/.dotfiles").homeConfigurations."parkin@galacticboi-nixos".options',
+                  expr = '(builtins.getFlake  "'
+                    .. dotfiles
+                    .. '").homeConfigurations."parkin@galacticboi-nixos".options',
                   -- expr = '(builtins.getFlake  "/home/parkin/.dotfiles").homeConfigurations."parkin@wsl-nixos".options',
                 },
               },
