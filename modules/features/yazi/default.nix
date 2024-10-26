7bc5368cdf3055f541c0e1d6dfba159f6539294b
Will Parkin <wmparkin@gmail.com>
2024-10-26 13:38:05 -0400

    use yazi flavor

--- modules/features/yazi/default.nix
+++ modules/features/yazi/default.nix
@@ -1,8 +1,18 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
+let
+  yaziFlavors = pkgs.fetchFromGitHub {
+    owner = "yazi-rs";
+    repo = "flavors";
+    rev = "main";
+    sha256 = "sha256-a9Ta0dLuxqay0TwcoAOzcQ0aqm40RyzFxXb25Qf8jcQ=";
+  };
+  myFlavor = "catppuccin-frappe";
+in
 {
   options = {
     myHomeManager.yazi.enable = lib.mkEnableOption "Enables yazi";
@@ -13,8 +23,15 @@
       enable = true;
       enableBashIntegration = true;
       shellWrapperName = "y";
+      flavors = {
+        ${myFlavor} = ''${yaziFlavors}/${myFlavor}.yazi'';
+      };
+      theme = {
+        flavor = {
+          use = "${myFlavor}";
+        };
+      };
     };
-
   };
 
 }
