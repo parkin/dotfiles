5db40a944c07a6888f67b35efae57883d5a5d29e
Will Parkin <wmparkin@gmail.com>
2024-10-31 21:09:39 -0400

    added fzf completion for zsh

--- modules/features/fzf.nix
+++ modules/features/fzf.nix
@@ -8,6 +8,8 @@
     pkgs.fzf
   ];
 
+  # set up fzf key bindings and fuzzy completion
   programs.bash.initExtra = "eval \"\$(fzf --bash)\"";
+  programs.zsh.initExtra = ''source <(fzf --zsh)'';
 
 }
