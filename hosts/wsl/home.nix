c1700423e50ebc0a2ca8f99e3504c7aa6a317de9
Will Parkin <wmparkin@gmail.com>
2024-11-01 17:37:32 -0400

    added BROWSER and plotly session variables for zsh

--- hosts/wsl/home.nix
+++ hosts/wsl/home.nix
@@ -8,6 +8,12 @@ let
   homeDirectory = "/home/parkin";
   dotfilesDir = ".dotfiles";
   dotfilesPath = "${homeDirectory}/${dotfilesDir}";
+  sessionVariables = {
+    # Set extra variables for Plotly to render in the Windows browser.
+    # see https://plotly.com/python/renderers/
+    BROWSER = ''/mnt/c/Program Files/Mozilla Firefox/firefox.exe'';
+    PLOTLY_RENDERER = "browser";
+  };
 in
 {
   # Home Manager needs a bit of information about you and the paths it should
@@ -18,12 +24,8 @@ in
   programs.git.userName = "Will Parkin";
   programs.git.userEmail = "parkin@ibm.com";
 
-  programs.bash.sessionVariables = {
-    # Set extra variables for Plotly to render in the Windows browser.
-    # see https://plotly.com/python/renderers/
-    BROWSER = ''/mnt/c/Program Files/Mozilla Firefox/firefox.exe'';
-    PLOTLY_RENDERER = "browser";
-  };
+  programs.bash.sessionVariables = sessionVariables;
+  programs.zsh.sessionVariables = sessionVariables;
 
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
