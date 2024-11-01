846f174e7eb989efe820a3f6730b00af7bfbf024
Will Parkin <wmparkin@gmail.com>
2024-10-31 21:04:38 -0400

    add eza aliases to bash and zsh

--- modules/features/eza.nix
+++ modules/features/eza.nix
@@ -1,19 +1,20 @@
 {
-  config,
   pkgs,
   ...
 }:
+let
+  shellAliases = {
+    ls = "eza --color=always --icons";
+    ll = "eza -la --icons --octal-permissions";
+    la = "eza --long --all --group";
+  };
+in
 {
   home.packages = [
     ## eza -- modern `ls`
     pkgs.eza
   ];
 
-  programs.bash = {
-    shellAliases = {
-      ls = "eza --color=always --icons";
-      ll = "eza -la --icons --octal-permissions";
-      la = "eza --long --all --group";
-    };
-  };
+  programs.bash.shellAliases = shellAliases;
+  programs.zsh.shellAliases = shellAliases;
 }
