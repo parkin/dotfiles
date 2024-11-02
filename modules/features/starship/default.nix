f6e51f8644366428195f6b45467e70351bb775b5
Will Parkin <wmparkin@gmail.com>
2024-11-01 23:14:56 -0400

    enabled Bash and Zsh integration for starship - fixed some extra text of the conda shell

--- modules/features/starship/default.nix
+++ modules/features/starship/default.nix
@@ -13,6 +13,8 @@
 
     programs.starship = {
       enable = true;
+      enableBashIntegration = true;
+      enableZshIntegration = true;
     };
     xdg.configFile."starship.toml" = {
       source = config.lib.file.mkOutOfStoreSymlink "${config.myHomeManager.dotfilesPath}/modules/features/starship/starship.toml";
