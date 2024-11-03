d2818da0b646ca6e636e4d342db2aa73f5bf8115
Will Parkin <wmparkin@gmail.com>
2024-11-03 11:22:07 -0500

    redid tmux conf to use programs.tmux. Enabled yank, catppuccin, resurrect, and continuum

--- modules/features/tmux/default.nix
+++ modules/features/tmux/default.nix
@@ -10,13 +10,49 @@
   };
   config = lib.mkIf config.myHomeManager.tmux.enable {
 
-    home.packages = with pkgs; [
-      tmux
-    ];
+    programs.tmux = {
+      enable = true;
+      baseIndex = 1;
+      shortcut = "b";
+      mouse = true;
+      terminal = "tmux-256color";
+      plugins = with pkgs.tmuxPlugins; [
+        yank
+        {
+          plugin = catppuccin;
+          extraConfig = ''
+            set -g @catppuccin_flavour "mocha"
+            set -g @catppuccin_window_tabs_enabled on
+            set -g @catppuccin_window_status_style "rounded"
+            set -g @catppuccin_date_time "%H:%M"
 
-    # copy the config file
-    home.file.".tmux.conf" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${config.myHomeManager.dotfilesPath}/modules/features/tmux/.tmux.conf";
+            # Make the status line pretty and add some modules
+            set -g status-right-length 100
+            set -g status-left-length 100
+            set -g status-left ""
+            set -g status-right "#{E:@catppuccin_status_application}"
+            set -ag status-right "#{E:@catppuccin_status_session}"
+          '';
+
+        }
+        {
+          plugin = resurrect;
+          extraConfig = ''
+            # set -g @resurrect-strategy-vim "session"
+            # set -g @resurrect-strategy-nvim "session"
+            set -g @resurrect-capture-pane-contents "on"
+          '';
+        }
+        {
+          plugin = continuum;
+          extraConfig = ''
+            set -g @continuum-restore "on"
+            set -g @continuum-boot "on"
+            set -g @continuum-save-interval "10"
+          '';
+        }
+      ];
+      extraConfig = (builtins.readFile ./.tmux.conf);
     };
   };
 }
