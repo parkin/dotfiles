d4ff83e33ca2cca40855d2e39e305bfa2c645168
Will Parkin <wmparkin@gmail.com>
2024-11-05 11:33:45 -0500

    added wsl-open

--- hosts/wsl/home.nix
+++ hosts/wsl/home.nix
@@ -51,9 +51,9 @@ in
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages =
-    [
-    ];
+  home.packages = with pkgs; [
+    wsl-open
+  ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
