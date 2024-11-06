00dd7fd56639b5504d326fde0da0586466860db9
Will Parkin <wmparkin@gmail.com>
2024-11-05 23:41:52 -0500

    switched to programs.eza to enable zsh integration, path completion for aliases wasn't working before

--- modules/features/eza.nix
+++ modules/features/eza.nix
@@ -1,19 +1,24 @@
 {
-  pkgs,
   ...
 }:
 let
+  ezaParams = " --git --group --icons --color=always";
   shellAliases = {
-    ls = "eza --color=always --icons";
-    ll = "eza -la --icons --octal-permissions";
-    la = "eza --long --all --group";
+    ls = "eza" + ezaParams;
+    l = "eza --git-ignore" + ezaParams;
+    ll = "eza -la --octal-permissions" + ezaParams;
+    la = "eza --long --all" + ezaParams;
+    lt = "eza --tree" + ezaParams;
+    tree = "eza --tree" + ezaParams;
   };
 in
 {
-  home.packages = [
-    ## eza -- modern `ls`
-    pkgs.eza
-  ];
+
+  programs.eza = {
+    enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+  };
 
   programs.bash.shellAliases = shellAliases;
   programs.zsh.shellAliases = shellAliases;
