be6e89761e5c901680ff8d7087e81c0c3f4b8c10
Will Parkin <wmparkin@gmail.com>
2025-04-15 22:41:02 -0400

    added audacity

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -42,6 +42,7 @@ in
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
+    audacity
     bitwarden-desktop
     megasync
     ## obsidian md
