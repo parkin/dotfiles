2d8ba5e6cc546dfeb8bff694ee38c6cb3e8cc321
Will Parkin <wmparkin@gmail.com>
2025-04-30 20:23:33 -0400

    switched back to iron.nvim

--- modules/features/neovim/nvim/lua/plugins/iron-nvim.lua
+++ modules/features/neovim/nvim/lua/plugins/iron-nvim.lua
@@ -1,72 +1,89 @@
-return {}
--- return {
---   -- "hkupty/iron.nvim", -- This is the original
---   "frere-jacques/iron.nvim",
---   branch = "feature_send_code_block", -- This is a fork that added code block running
---   init = function()
---     local iron = require("iron.core")
---
---     iron.setup({
---       config = {
---         -- Whether a repl should be discarded or not
---         scratch_repl = true,
---         -- Your repl definitions come here
---         repl_definition = {
---           sh = {
---             -- Can be a table or a function that
---             -- returns a table (see below)
---             command = { "zsh" },
---           },
---           python = {
---             -- command = { "python3" }, -- or { "ipython", "--no-autoindent" }
---             command = {
---               "jupyter-console",
---               -- "--existing",
---               "--ZMQTerminalInteractiveShell.image_handler=None",
---             },
---             format = require("iron.fts.common").bracketed_paste_python,
---             block_deviders = { "# %%", "#%%" },
---           },
---         },
---         -- How the repl window will be displayed
---         -- See below for more information
---         -- repl_open_cmd = require("iron.view").bottom(40),
---         repl_open_cmd = "vertical botright 80 split",
---       },
---       -- Iron doesn't set keymaps by default anymore.
---       -- You can set them here or manually add keymaps to the functions in iron.core
---       keymaps = {
---         send_motion = "<leader>ic",
---         visual_send = "<leader>ic",
---         send_file = "<leader>if",
---         send_line = "<leader>il",
---         send_paragraph = "<leader>ip",
---         send_until_cursor = "<leader>iu",
---         send_mark = "<leader>im",
---         send_code_block = "<leader>ib",
---         send_code_block_and_move = "<leader>in",
---         mark_motion = "<leader>imc",
---         mark_visual = "<leader>imc",
---         remove_mark = "<leader>imd",
---         cr = "<leader>i<cr>",
---         interrupt = "<leader>i<leader>",
---         exit = "<leader>iq",
---         clear = "<leader>icl",
---       },
---       -- If the highlight is on, you can change how it looks
---       -- For the available options, check nvim_set_hl
---       highlight = {
---         italic = true,
---       },
---       ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
---     })
---
---     -- iron also has a list of commands, see :h iron-commands for all available commands
---     local wk = require("which-key")
---     wk.add({ "<leader>i", group = "Iron repl" })
---     vim.keymap.set("n", "<leader>ii", "<cmd>IronRepl<cr>")
---     vim.keymap.set("n", "<leader>ir", "<cmd>IronRestart<cr>")
---     vim.keymap.set("n", "<leader>if", "<cmd>IronFocus<cr>")
---     vim.keymap.set("n", "<leader>ih", "<cmd>IronHide<cr>")
---   end,
--- }
+return {
+  -- "hkupty/iron.nvim", -- This is the original
+  "Vigemus/iron.nvim",
+  -- "frere-jacques/iron.nvim",
+  -- branch = "feature_send_code_block", -- This is a fork that added code block running
+  init = function()
+    local iron = require("iron.core")
+    local view = require("iron.view")
+    local common = require("iron.fts.common")
+
+    iron.setup({
+      config = {
+        -- Whether a repl should be discarded or not
+        scratch_repl = true,
+        -- Your repl definitions come here
+        repl_definition = {
+          sh = {
+            -- Can be a table or a function that
+            -- returns a table (see below)
+            command = { "zsh" },
+          },
+          -- python = {
+          --   command = { "ipython", "--no-autoindent" }, -- { "python3" }
+          --   format = common.bracketed_paste_python,
+          --   block_dividers = { "# %%", "#%%" },
+          -- },
+          python = {
+            -- command = { "python3" }, -- or { "ipython", "--no-autoindent" }
+            command = {
+              "jupyter-console",
+              -- "--existing",
+              "--ZMQTerminalInteractiveShell.image_handler=None",
+            },
+            format = require("iron.fts.common").bracketed_paste_python,
+            block_deviders = { "# %%", "#%%" },
+          },
+        },
+        -- set the file type of the newly created repl to ft
+        -- bufnr is the buffer id of the REPL and ft is the filetype of the
+        -- language being used for the REPL.
+        repl_filetype = function(bufnr, ft)
+          return ft
+          -- or return a string name such as the following
+          -- return "iron"
+        end,
+        -- How the repl window will be displayed
+        -- See below for more information
+        -- repl_open_cmd = view.bottom(40),
+        repl_open_cmd = view.split.vertical.botright(50),
+      },
+      -- Iron doesn't set keymaps by default anymore.
+      -- You can set them here or manually add keymaps to the functions in iron.core
+      keymaps = {
+        toggle_repl = "<space>ir", -- toggles the repl open and closed.
+        -- If repl_open_command is a table as above, then the following keymaps are
+        -- available
+        -- toggle_repl_with_cmd_1 = "<space>rv",
+        -- toggle_repl_with_cmd_2 = "<space>rh",
+        restart_repl = "<space>iR", -- calls `IronRestart` to restart the repl
+        send_motion = "<space>ic",
+        visual_send = "<space>iv",
+        send_file = "<space>if",
+        send_line = "<space>il",
+        send_paragraph = "<space>ip",
+        send_until_cursor = "<space>iu",
+        send_mark = "<space>im",
+        send_code_block = "<space>ib",
+        send_code_block_and_move = "<space>in",
+        mark_motion = "<space>imc",
+        mark_visual = "<space>imc",
+        remove_mark = "<space>imd",
+        cr = "<space>s<cr>",
+        interrupt = "<space>ii",
+        exit = "<space>iq",
+        clear = "<space>ix",
+      },
+      -- If the highlight is on, you can change how it looks
+      -- For the available options, check nvim_set_hl
+      highlight = {
+        italic = true,
+      },
+      ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
+    })
+
+    -- iron also has a list of commands, see :h iron-commands for all available commands
+    vim.keymap.set("n", "<space>rf", "<cmd>IronFocus<cr>")
+    vim.keymap.set("n", "<space>rh", "<cmd>IronHide<cr>")
+  end,
+}
