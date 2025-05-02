3808d579c5f285ea97d224bd2b2650fb81d3303f
Will Parkin <wmparkin@gmail.com>
2025-05-02 17:39:14 -0400

    fixed yanking to clipboard on wsl

--- modules/features/neovim/nvim/lua/config/options.lua
+++ modules/features/neovim/nvim/lua/config/options.lua
@@ -3,3 +3,22 @@
 -- Add any additional options here
 
 vim.opt.scrolloff = 12
+
+-- Clipboard options, only for my WSL
+if vim.fn.hostname() == "wsl-nixos" then
+  if vim.fn.has("wsl") == 1 then
+    vim.g.clipboard = {
+      name = "WslClipboard",
+      -- need to explicitly set clip as the copy utility here
+      copy = {
+        ["+"] = "/mnt/c/Windows/System32/clip.exe",
+        ["*"] = "/mnt/c/Windows/System32/clip.exe",
+      },
+      -- paste = {
+      --   ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      --   ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      -- },
+      cache_enabled = 0,
+    }
+  end
+end
