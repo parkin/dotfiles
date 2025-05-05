e5e3407dbb9692c0887e2a1afe4c67dc9ca6eb5a
Will Parkin <wmparkin@gmail.com>
2025-05-05 08:30:58 -0400

    added comment to explain search

--- modules/features/neovim/nvim/ftplugin/python.lua
+++ modules/features/neovim/nvim/ftplugin/python.lua
@@ -10,6 +10,7 @@ vim.keymap.set(
   -- ^ - beginning of line
   -- [ \\t]* any number of spaces or tabs
   -- #\\s8%% - # %% with any number of spaces between
+  -- <Cmd>nohlsearch<CR> removes the highlighting from the search
   "/^[ \\t]*#\\s*%%<CR><Cmd>nohlsearch<CR>",
   { desc = "Move to next code block", noremap = true, silent = true, buffer = true }
 )
