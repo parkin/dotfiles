6c6ed52aed9dba2ad07574ae9f15bca13f92caa0
Will Parkin <wmparkin@gmail.com>
2025-05-06 11:59:19 -0400

    hopefully fix neovim clipboard yank on wsl

--- modules/features/neovim/nvim/lua/config/options.lua
+++ modules/features/neovim/nvim/lua/config/options.lua
@@ -6,19 +6,17 @@ vim.opt.scrolloff = 12
 
 -- Clipboard options, only for my WSL
 if vim.fn.hostname() == "wsl-nixos" then
-  if vim.fn.has("wsl") == 1 then
-    vim.g.clipboard = {
-      name = "WslClipboard",
-      -- need to explicitly set clip as the copy utility here
-      copy = {
-        ["+"] = "/mnt/c/Windows/System32/clip.exe",
-        ["*"] = "/mnt/c/Windows/System32/clip.exe",
-      },
-      -- paste = {
-      --   ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-      --   ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-      -- },
-      cache_enabled = 0,
-    }
-  end
+  vim.g.clipboard = {
+    name = "WslClipboard",
+    copy = {
+      ["+"] = "/mnt/c/Windows/System32/clip.exe",
+      ["*"] = "/mnt/c/Windows/System32/clip.exe",
+    },
+    paste = {
+      -- TODO: fix pwsh location for wsl
+      ["+"] = 'pwsh.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ["*"] = 'pwsh.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+  }
 end
