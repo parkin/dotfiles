0c07dab2d38b78ac00ecb4ba6d826568434a71dc
Will Parkin <wmparkin@gmail.com>
2025-05-09 01:02:45 -0400

    first wca adapter

--- modules/features/neovim/nvim/lua/plugins/codecompanion.lua
+++ modules/features/neovim/nvim/lua/plugins/codecompanion.lua
@@ -4,6 +4,7 @@ wk.add({
   { "<leader>a", group = "+AI" }, -- group
 })
 
+-- my usual config
 return {
   "olimorris/codecompanion.nvim",
   dependencies = {
@@ -13,14 +14,18 @@ return {
   opts = {
     strategies = {
       chat = {
-        adapter = "anthropic2",
+        -- adapter = "anthropic2",
+        adapter = "wca",
       },
-      inline = {
-        adapter = "anthropic2",
-      },
-      cmd = {
-        adapter = "anthropic2",
-      },
+      -- inline = {
+      --   adapter = "anthropic2",
+      -- },
+      -- cmd = {
+      --   adapter = "anthropic2",
+      -- },
+    },
+    opts = {
+      log_level = "DEBUG", -- For development
     },
     adapters = {
       anthropic2 = function()
@@ -29,6 +34,179 @@ return {
           schema = {
             max_tokens = {
               default = 30000,
+            },
+          },
+        })
+      end,
+      wca = function()
+        local client = require("codecompanion.http")
+        local openai = require("codecompanion.adapters.openai")
+        local log = require("codecompanion.utils.log")
+        local curl = require("plenary.curl")
+
+        ---@alias WCABearerToken {access_token: string, expiration: number}|nil
+        local _iam_token
+
+        -- Note for this to work, I had to edit the requests in the "codecompanion.http library" to use form instead of body
+        -- -- body = body_file.filename or "",
+        -- form = { message = vim.base64.encode(body) },
+
+        ---@return WCABearerToken
+        local function authorize_token()
+          if _iam_token and _iam_token.expiration > os.time() then
+            log:trace("Reusing IAM token")
+            return _iam_token
+          end
+
+          log:debug("Requesting new IAM token")
+
+          local wca_api_key = os.getenv("WCA_API_KEY")
+
+          local request = curl.post("https://iam.cloud.ibm.com/identity/token", {
+            headers = { ["Content-Type"] = "application/x-www-form-urlencoded" },
+            body = "grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey=" .. wca_api_key,
+            on_error = function(err)
+              log:error("WCA Adapter: Error requesting IAM token, %s", err)
+            end,
+          })
+
+          _iam_token = vim.json.decode(request.body)
+          return _iam_token
+        end
+
+        ---@class WCA.Adapter: CodeCompanion.Adapter
+        return require("codecompanion.adapters").new({
+          name = "wca",
+          formatted_name = "Watson Code Assistant",
+          roles = {
+            llm = "assistant",
+            user = "user",
+          },
+          opts = {
+            stream = false,
+          },
+          features = {
+            text = true,
+            tokens = true,
+            vision = false,
+          },
+          url = "https://api.dataplatform.cloud.ibm.com/v2/wca/core/chat/text/generation",
+          env = {
+            api_key = function()
+              return authorize_token().access_token
+            end,
+          },
+          headers = {
+            Authorization = "Bearer ${api_key}",
+            ["content-type"] = "multipart/form-data",
+          },
+          handlers = {
+            setup = function(self)
+              if self.opts and self.opts.stream then
+                self.parameters.stream = true
+              end
+              return true
+            end,
+
+            --- Use the OpenAI adapter for the bulk of the work
+            tokens = function(self, data)
+              return openai.handlers.tokens(self, data)
+            end,
+            form_parameters = function(self, params, messages)
+              return openai.handlers.form_parameters(self, params, messages)
+            end,
+            form_messages = function(self, messages)
+              messages = vim
+                .iter(messages)
+                :map(function(m)
+                  local model = self.schema.model.default
+                  if type(model) == "function" then
+                    model = model(self)
+                  end
+                  if vim.startswith(model, "o1") and m.role == "system" then
+                    m.role = self.roles.user
+                  end
+
+                  -- Ensure tool_calls are clean
+                  if m.tool_calls then
+                    m.tool_calls = vim
+                      .iter(m.tool_calls)
+                      :map(function(tool_call)
+                        return {
+                          id = tool_call.id,
+                          ["function"] = tool_call["function"],
+                          type = tool_call.type,
+                        }
+                      end)
+                      :totable()
+                  end
+
+                  return {
+                    role = m.role:upper(), -- ensure upper case for WCA
+                    content = m.content,
+                    tool_calls = m.tool_calls,
+                    tool_call_id = m.tool_call_id,
+                  }
+                end)
+                :totable()
+
+              return { messages = messages }
+            end,
+            set_body = function(self, payload)
+              local output = { message_payload = openai.handlers.form_messages(self, payload.messages) }
+              return output
+            end,
+            ---Output the data from the API ready for insertion into the chat buffer
+            ---@param self CodeCompanion.Adapter
+            ---@param data table The streamed JSON data from the API, also formatted by the format_data handler
+            ---@return table|nil [status: string, output: table]
+            chat_output = function(self, data)
+              log:debug("chat_output: " .. vim.json.encode(data))
+
+              if not data or data == "" then
+                log:error("No data passed to chat_output")
+                return nil
+              end
+
+              -- Handle only structured response
+              local data_mod = type(data) == "table" and data.body
+              local ok, json = pcall(vim.json.decode, data_mod, { luanil = { object = true } })
+
+              if not ok or not json.response then
+                log:error("error with json decode pcall")
+                return nil
+              end
+
+              local output = {
+                role = json.response.message.role:lower() or "",
+                content = json.response.message.content:lower() or "",
+              }
+
+              self.schema.model.default = json.model
+
+              return {
+                status = "success",
+                output = output,
+              }
+            end,
+            inline_output = function(self, data, context)
+              return openai.handlers.inline_output(self, data, context)
+            end,
+            on_exit = function(self, data)
+              return openai.handlers.on_exit(self, data)
+            end,
+          },
+          schema = {
+            ---@type CodeCompanion.Schema
+            model = {
+              order = 1,
+              mapping = "parameters",
+              type = "enum",
+              desc = "ID of the model to use. See the model endpoint compatibility table for details on which models work with the Chat API.",
+              default = "wca?",
+              choices = {
+                "wca?",
+              },
             },
           },
         })
