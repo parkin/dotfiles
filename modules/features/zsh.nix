77edb2609bced67c23fd4b603ec2371aecbf9cf2
Will Parkin <wmparkin@gmail.com>
2025-05-18 00:24:39 -0400

    added plugin to sync zsh completions so that completions will work for things installed by direnv

--- modules/features/zsh.nix
+++ modules/features/zsh.nix
@@ -1,6 +1,7 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }:
 {
@@ -18,6 +19,21 @@
         size = 10000;
       };
       shellAliases = config.myHomeManager.terminal.shellAliases;
+      plugins = [
+        # see this issue with direnv and shell completions: https://github.com/direnv/direnv/issues/443
+        # this plugin syncs zsh completions when new ones are added
+        # which is needed for completions to work for things installed by direnv.
+        # https://github.com/BronzeDeer/zsh-completion-sync
+        {
+          name = "zsh-completion-sync";
+          src = pkgs.fetchFromGitHub {
+            owner = "BronzeDeer";
+            repo = "zsh-completion-sync";
+            rev = "f6e95baf8cd87d9065516d1fa0bf0cb33b4235f3";
+            sha256 = "sha256-XhZ7l8e2H1+W1oUkDrr8pQVPVbb3+1/wuu7MgXsTs+8=";
+          };
+        }
+      ];
     };
   };
 
