f6f2e1835cefc7593f42bbfaffbc5cad2ce51fc5
Will Parkin <wmparkin@gmail.com>
2025-05-21 17:13:22 -0400

    updated codecompanion so wca now works with main codecompanion branch. also, lazy packages updated.

--- modules/features/neovim/nvim/lua/plugins/codecompanion.lua
+++ modules/features/neovim/nvim/lua/plugins/codecompanion.lua
@@ -11,9 +11,7 @@ end
 
 -- my usual config
 return {
-  -- "olimorris/codecompanion.nvim",
-  "parkin/codecompanion.nvim",
-  branch = "request_override",
+  "olimorris/codecompanion.nvim",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
@@ -45,7 +43,7 @@ return {
         })
       end,
       wca = function()
-        local client = require("codecompanion.http")
+        -- local client = require("codecompanion.http")
         local openai = require("codecompanion.adapters.openai")
         local log = require("codecompanion.utils.log")
         local curl = require("plenary.curl")
@@ -86,6 +84,212 @@ return {
           },
           opts = {
             stream = false,
+            -- TODO: streamline this request and handler logic (for now, just copied from main CodeCompanion)
+            request = function(client, payload, actions, opts)
+              local config = require("codecompanion.config")
+              local util = require("codecompanion.utils")
+              local Path = require("plenary.path")
+
+              opts = opts or {}
+              local cb = log:wrap_cb(actions.callback, "Response error: %s") --[[@type function]]
+
+              -- Make a copy of the adapter to ensure that we replace variables in every request
+              local adapter = vim.deepcopy(client.adapter)
+
+              local handlers = adapter.handlers
+
+              if handlers and handlers.setup then
+                local ok = handlers.setup(adapter)
+                if not ok then
+                  return log:error("Failed to setup adapter")
+                end
+              end
+
+              adapter:get_env_vars()
+
+              local body = client.opts.encode(
+                vim.tbl_extend(
+                  "keep",
+                  handlers.form_parameters
+                      and handlers.form_parameters(adapter, adapter:set_env_vars(adapter.parameters), payload.messages)
+                    or {},
+                  handlers.form_messages and handlers.form_messages(adapter, payload.messages) or {},
+                  handlers.form_tools and handlers.form_tools(adapter, payload.tools) or {},
+                  adapter.body and adapter.body or {},
+                  handlers.set_body and handlers.set_body(adapter, payload) or {}
+                )
+              )
+
+              local body_file = Path.new(vim.fn.tempname() .. ".json")
+              body_file:write(vim.split(body, "\n"), "w")
+
+              log:info("Request body file: %s", body_file.filename)
+
+              ---@type table|nil
+              local form = handlers.set_form and handlers.set_form(adapter, payload) or nil
+
+              local form_files = {} -- keep track of files so they can be deleted
+              local form_filenames = {} -- final { form_name = "<filename" } table for curl
+              -- add temporary files for each form entry
+              if form then
+                for name, content in pairs(form) do
+                  -- first check that content isn't already referencing a file with leading "@" or "<" character
+                  if content:sub(1, 1) ~= "@" and content:sub(1, 1) ~= "<" then
+                    -- make a temporary file
+                    local form_file = Path.new(vim.fn.tempname())
+                    form_file:write(vim.split(content, "\n"), "w")
+                    form_files[name] = form_file
+                    log:info("Request form: %s: %s", name, form_file.filename)
+
+                    -- add the entry for curl
+                    form_filenames[name] = "<" .. form_file.filename
+                  else
+                    form_filenames[name] = content
+                  end
+                end
+              end
+
+              local function cleanup(status)
+                if vim.tbl_contains({ "ERROR", "INFO" }, config.opts.log_level) and status ~= "error" then
+                  body_file:rm()
+
+                  -- cleanup the form temp files
+                  for _, form_file in pairs(form_files) do
+                    form_file:rm()
+                  end
+                end
+              end
+
+              local raw = {
+                "--retry",
+                "3",
+                "--retry-delay",
+                "1",
+                "--keepalive-time",
+                "60",
+                "--connect-timeout",
+                "10",
+              }
+
+              if adapter.opts and adapter.opts.stream then
+                table.insert(raw, "--tcp-nodelay")
+                table.insert(raw, "--no-buffer")
+              end
+
+              if adapter.raw then
+                vim.list_extend(raw, adapter:set_env_vars(adapter.raw))
+              end
+
+              local request_opts = {
+                url = adapter:set_env_vars(adapter.url),
+                headers = adapter:set_env_vars(adapter.headers),
+                insecure = config.adapters.opts.allow_insecure,
+                proxy = config.adapters.opts.proxy,
+                raw = raw,
+                body = body_file.filename or "",
+                form = form and form_filenames or nil,
+                -- This is called when the request is finished. It will only ever be called
+                -- once, even if the endpoint is streaming.
+                callback = function(data)
+                  vim.schedule(function()
+                    if (not adapter.opts.stream) and data and data ~= "" then
+                      log:trace("Output data:\n%s", data)
+                      cb(nil, data, adapter)
+                    end
+                    if handlers and handlers.on_exit then
+                      handlers.on_exit(adapter, data)
+                    end
+                    if handlers and handlers.teardown then
+                      handlers.teardown(adapter)
+                    end
+                    if actions.done and type(actions.done) == "function" then
+                      actions.done()
+                    end
+
+                    opts.status = "success"
+                    if data.status >= 400 then
+                      opts.status = "error"
+                    end
+
+                    if not opts.silent then
+                      util.fire("RequestFinished", opts)
+                    end
+                    cleanup(opts.status)
+                    if client.user_args.event then
+                      if not opts.silent then
+                        util.fire("RequestFinished" .. (client.user_args.event or ""), opts)
+                      end
+                    end
+                  end)
+                end,
+                on_error = function(err)
+                  vim.schedule(function()
+                    actions.callback(err, nil)
+                    if not opts.silent then
+                      util.fire("RequestFinished", opts)
+                    end
+                  end)
+                end,
+              }
+
+              -- allow the adapter to modify the http request before it is sent
+              request_opts = handlers.modify_request_opts
+                  and handlers.modify_request_opts(client, payload, request_opts)
+                or request_opts
+
+              if adapter.opts and adapter.opts.stream then
+                local has_started_steaming = false
+
+                -- Turn off plenary's default compression
+                request_opts["compressed"] = adapter.opts.compress or false
+
+                -- This will be called multiple times until the stream is finished
+                request_opts["stream"] = client.opts.schedule(function(_, data)
+                  if data and data ~= "" then
+                    log:trace("Output data:\n%s", data)
+                  end
+                  if not has_started_steaming then
+                    has_started_steaming = true
+                    if not opts.silent then
+                      util.fire("RequestStreaming", opts)
+                    end
+                  end
+                  cb(nil, data, adapter)
+                end)
+              end
+
+              local request = "post"
+              if adapter.opts and adapter.opts.method then
+                request = adapter.opts.method:lower()
+              end
+
+              local job = client.opts[request](request_opts)
+
+              -- Data to be sent via the request
+              opts.id = math.random(10000000)
+              opts.adapter = {
+                name = adapter.name,
+                formatted_name = adapter.formatted_name,
+                model = type(adapter.schema.model.default) == "function" and adapter.schema.model.default()
+                  or adapter.schema.model.default
+                  or "",
+              }
+
+              if not opts.silent then
+                util.fire("RequestStarted", opts)
+              end
+
+              if job and job.args then
+                log:debug("Request:\n%s", job.args)
+              end
+              if client.user_args.event then
+                if not opts.silent then
+                  util.fire("RequestStarted" .. (client.user_args.event or ""), opts)
+                end
+              end
+
+              return job
+            end,
           },
           features = {
             text = true,
