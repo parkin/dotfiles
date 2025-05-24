3844fccd2fced698abfb0542b48726c7964c2b41
Will Parkin <wmparkin@gmail.com>
2025-05-23 23:47:49 -0400

    lowercase

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   # https://github.com/Misterio77/nix-starter-configs
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
