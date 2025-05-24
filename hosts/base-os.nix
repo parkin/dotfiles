3cad654b479be27dad22be1b0fe3166159e43a00
Will Parkin <wmparkin@gmail.com>
2025-05-23 23:48:08 -0400

    added experimental-features to the os level

--- hosts/base-os.nix
+++ hosts/base-os.nix
@@ -13,5 +13,10 @@ in
     # NOTE: potential workaround for sessionVariables setting not working with home manager
     # https://github.com/nix-community/home-manager/issues/1011
     environment.extraInit = "[[ -f ${homeManagerSessionVars} ]] && source ${homeManagerSessionVars}";
+    # enable flake support at os level
+    nix.settings.experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
   };
 }
