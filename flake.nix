6e5078a293acc37c78de97e3541d068c795a2990
Will Parkin <wmparkin@gmail.com>
2025-05-23 23:21:04 -0400

    updated flake to 25.05

--- flake.nix
+++ flake.nix
@@ -5,10 +5,10 @@
   # https://github.com/Misterio77/nix-starter-configs
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
