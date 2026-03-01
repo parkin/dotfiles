0bf891bd23e93f367b8159b75505e85107a7f322
Will Parkin <parkin@ibm.com>
2026-03-01 10:16:15 -0500

    setup for nvidia gpu on dell-wsl. It is working but Im not sure if this is the correct approach

--- hosts/dell-wsl-nixos/configuration.nix
+++ hosts/dell-wsl-nixos/configuration.nix
@@ -46,6 +46,14 @@
     fsType = "drvfs";
   };
 
+  # enable OpenGL / GPU Support
+  hardware.graphics.enable = true;
+  hardware.opengl.enable = true;
+
+  wsl.useWindowsDriver = true;
+
+  # Make the WSL CUDA libraries available system-wide
+
   # needed to use this to get pixi envs to work
   programs.nix-ld.enable = true;
 
@@ -61,10 +69,48 @@
   system.stateVersion = "25.05"; # Did you read the comment?
 
   environment.systemPackages = with pkgs; [
+    # for WSL CUDA gpu libraries
+    cudatoolkit
     # add an editor in case we can't access the user for some reason
     neovim
     # WSL utilities
     wslu
   ];
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia.open = true;
+
+  environment.sessionVariables = {
+    CUDA_PATH = "${pkgs.cudatoolkit}";
+    EXTRA_LDFLAGS = "-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib";
+    EXTRA_CCFLAGS = "-I/usr/include";
+    LD_LIBRARY_PATH = [
+      "/usr/lib/wsl/lib"
+      "${pkgs.linuxPackages.nvidia_x11}/lib"
+      "${pkgs.ncurses5}/lib"
+    ];
+    MESA_D3D12_DEFAULT_ADAPTER_NAME = "Nvidia";
+  };
+
+  hardware.nvidia-container-toolkit = {
+    enable = true;
+    mount-nvidia-executables = false;
+  };
+
+  systemd.services = {
+    nvidia-cdi-generator = {
+      description = "Generate nvidia cdi";
+      wantedBy = [ "docker.service" ];
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.nvidia-docker}/bin/nvidia-ctk cdi generate --output=/etc/cdi/nvidia.yaml --nvidia-ctk-path=${pkgs.nvidia-container-toolkit}/bin/nvidia-ctk";
+      };
+    };
+  };
+
+  virtualisation.docker = {
+    daemon.settings.features.cdi = true;
+    daemon.settings.cdi-spec-dirs = [ "/etc/cdi" ];
+  };
 
 }
