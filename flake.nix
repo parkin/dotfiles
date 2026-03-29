3d9426e6e4a8034901510d612e23d59bc70dc1b8
Will Parkin <wmparkin@gmail.com>
2026-03-28 23:22:30 -0400

    bumped taskeru version

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     };
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
     taskeru = {
-      url = "git+ssh://git@github.com/parkin/taskeru.git?ref=refs/tags/v0.29.1";
+      url = "git+ssh://git@github.com/parkin/taskeru.git?ref=refs/tags/v0.30.0";
     };
     disko = {
       url = "github:nix-community/disko";
