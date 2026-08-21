5227ef7978ae2f8d512bbb67fa65a3594d8c4db0
Will Parkin <parkin@ibm.com>
2026-08-20 20:05:13 -0400

    added git lfs extension

--- modules/bundles/terminal.nix
+++ modules/bundles/terminal.nix
@@ -110,6 +110,7 @@
 
     programs.git = {
       enable = true;
+      lfs.enable = true;
       # set the git userName and userEmail in the host's home.nix
       # userName = "Will Parkin";
       # userEmail = "wmparkin@gmail.com";
