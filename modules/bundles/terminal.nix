a723e0d489a2dce2103f31963ea9b70cd28f3d7d
Will Parkin <wmparkin@gmail.com>
2026-09-02 07:08:37 -0400

    no ssh for gpg for now

--- modules/bundles/terminal.nix
+++ modules/bundles/terminal.nix
@@ -134,10 +134,7 @@
     programs.gpg.enable = true;
     services.gpg-agent = {
       enable = true;
-      enableSshSupport = true; # use your GPG auth subkey as an SSH key
-      pinentry.package = pkgs.pinentry-curses; # or pinentry-gnome3, pinentry-qt, pinentry-mac
-      defaultCacheTtl = 3600;
-      maxCacheTtl = 86400;
+      pinentry.package = pkgs.pinentry-curses;
     };
   };
 
