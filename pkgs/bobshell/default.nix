0de333b8e0198c7bc21030855296bdd42022e8af
Will Parkin <parkin@ibm.com>
2026-09-21 08:57:26 -0400

    updated bobshell to v2.0.4

--- pkgs/bobshell/default.nix
+++ pkgs/bobshell/default.nix
@@ -30,11 +30,11 @@ let
 in
 pkgs.stdenv.mkDerivation rec {
   pname = "bobshell";
-  version = "2.0.2";
+  version = "2.0.4";
 
   src = fetchurl {
     url = "https://s3.us-south.cloud-object-storage.appdomain.cloud/bob-shell/bobshell-${version}.tgz";
-    sha256 = "sha256-/6+BX1GENLPXfCEBGCgy1z8FN3B8KL5gRPoZaEEIAac=";
+    sha256 = "sha256-EN4Ef/3COlDz4e9p+tO2MT/23aABBYmiLv4mskaFJUs=";
   };
 
   nativeBuildInputs = [
