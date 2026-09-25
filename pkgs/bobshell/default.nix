7f27d67394a94c8180aed0e29d3f6b6f25f9f7b6
Will Parkin <parkin@ibm.com>
2026-09-24 21:59:22 -0400

    updated bobshell to v2.0.5

--- pkgs/bobshell/default.nix
+++ pkgs/bobshell/default.nix
@@ -21,12 +21,111 @@ let
     stripRoot = false;
   };
 
-  # Fetch the platform-specific binary for linux-x64
-  nodePtyLinuxX64 = fetchzip {
-    url = "https://registry.npmjs.org/@lydell/node-pty-linux-x64/-/node-pty-linux-x64-1.1.0.tgz";
-    hash = "sha256-xAFMHyBpWIF6L3z7dcnuZYyhFKnjKzGmrpaCPxzfi2Y=";
+  # Platform-specific node-pty binary package
+  nodePtyPlatform =
+    let
+      sources = {
+        "x86_64-linux" = {
+          url = "https://registry.npmjs.org/@lydell/node-pty-linux-x64/-/node-pty-linux-x64-1.1.0.tgz";
+          hash = "sha256-xAFMHyBpWIF6L3z7dcnuZYyhFKnjKzGmrpaCPxzfi2Y=";
+          pkgName = "node-pty-linux-x64";
+        };
+        "aarch64-linux" = {
+          url = "https://registry.npmjs.org/@lydell/node-pty-linux-arm64/-/node-pty-linux-arm64-1.1.0.tgz";
+          hash = "sha256-8bQx4XqKZHl4ZDWpEvEBXQIeWVc9L+Vb4TYo4cNogJM=";
+          pkgName = "node-pty-linux-arm64";
+        };
+        "x86_64-darwin" = {
+          url = "https://registry.npmjs.org/@lydell/node-pty-darwin-x64/-/node-pty-darwin-x64-1.1.0.tgz";
+          hash = "sha256-ANCMTH7O1QyX/YjUbEcMl4up0SX92oQbsBCoRJuYiI4=";
+          pkgName = "node-pty-darwin-x64";
+        };
+        "aarch64-darwin" = {
+          url = "https://registry.npmjs.org/@lydell/node-pty-darwin-arm64/-/node-pty-darwin-arm64-1.1.0.tgz";
+          hash = "sha256-6CF3sfXTMhd8FDAb6B3/hXaMI3ui6GAiOGfgKqY+ifU=";
+          pkgName = "node-pty-darwin-arm64";
+        };
+      };
+      srcInfo =
+        sources.${pkgs.stdenv.hostPlatform.system}
+          or (throw "Unsupported system for node-pty: ${pkgs.stdenv.hostPlatform.system}");
+    in
+    {
+      inherit (srcInfo) pkgName;
+      src = fetchzip {
+        url = srcInfo.url;
+        hash = srcInfo.hash;
+        stripRoot = false;
+      };
+    };
+
+  # Fetch the @officecli/officecli npm package
+  officecli = fetchzip {
+    url = "https://registry.npmjs.org/@officecli/officecli/-/officecli-1.0.144.tgz";
+    hash = "sha256-lCPl3wPbY+KRFUe/a6Hchkw7566Oox3qX3dtjVmXicg=";
     stripRoot = false;
   };
+
+  # Platform-specific officecli binary
+  officecliBinary =
+    let
+      binaries = {
+        "x86_64-linux" = fetchurl {
+          url = "https://github.com/iOfficeAI/OfficeCLI/releases/download/v1.0.144/officecli-linux-x64";
+          hash = "sha256-Mu96IaVKTKbJgGv16fPTK/sSkQFzKcVQRMsqrHGCLrg=";
+        };
+        "aarch64-linux" = fetchurl {
+          url = "https://github.com/iOfficeAI/OfficeCLI/releases/download/v1.0.144/officecli-linux-arm64";
+          hash = "sha256-VuwsMRS2b2SQiItneMu4QTplkRomysxyB/KeE0JJZto=";
+        };
+        "x86_64-darwin" = fetchurl {
+          url = "https://github.com/iOfficeAI/OfficeCLI/releases/download/v1.0.144/officecli-mac-x64";
+          hash = "sha256-NmEAZD11ew2iSClCKJfKdHaKiUtezRpHGhM2+OKgeH0=";
+        };
+        "aarch64-darwin" = fetchurl {
+          url = "https://github.com/iOfficeAI/OfficeCLI/releases/download/v1.0.144/officecli-mac-arm64";
+          hash = "sha256-BHVxY0KMW96Nkej4OFF4GOdHIhV3IspfOHe2cWt3vUU=";
+        };
+      };
+    in
+    binaries.${pkgs.stdenv.hostPlatform.system}
+      or (throw "Unsupported system for officecli: ${pkgs.stdenv.hostPlatform.system}");
+
+  # Fetch the @vscode/ripgrep npm package
+  vscodeRipgrep = fetchzip {
+    url = "https://registry.npmjs.org/@vscode/ripgrep/-/ripgrep-1.17.1.tgz";
+    hash = "sha256-tRnAUK4wTxRhYl7ZIkIEIjFCmiCSIMyE7guCEjbXBDI=";
+    stripRoot = false;
+  };
+
+  # Platform-specific ripgrep binary for @vscode/ripgrep
+  vscodeRipgrepBinary =
+    let
+      sources = {
+        "x86_64-linux" = fetchzip {
+          url = "https://github.com/microsoft/ripgrep-prebuilt/releases/download/v15.0.1/ripgrep-v15.0.1-x86_64-unknown-linux-musl.tar.gz";
+          hash = "sha256-lFLt8/eUPYlJgIFG2mBrpTV2L2+JSOEU1BBOMrmzeQU=";
+          stripRoot = false;
+        };
+        "aarch64-linux" = fetchzip {
+          url = "https://github.com/microsoft/ripgrep-prebuilt/releases/download/v15.0.1/ripgrep-v15.0.1-aarch64-unknown-linux-musl.tar.gz";
+          hash = "sha256-3Tc4pLbo3w+zvD7cWvNSxMOeDZetEYoj5Rdr3F1Iugg=";
+          stripRoot = false;
+        };
+        "x86_64-darwin" = fetchzip {
+          url = "https://github.com/microsoft/ripgrep-prebuilt/releases/download/v15.0.1/ripgrep-v15.0.1-x86_64-apple-darwin.tar.gz";
+          hash = "sha256-WRxpPoC7RE7xkHsqkG/rnHe8r+HN9QkQfMddzw6HW9I=";
+          stripRoot = false;
+        };
+        "aarch64-darwin" = fetchzip {
+          url = "https://github.com/microsoft/ripgrep-prebuilt/releases/download/v15.0.1/ripgrep-v15.0.1-aarch64-apple-darwin.tar.gz";
+          hash = "sha256-L6FkZP2GOFiKZ8f8Fy08S1f73GXf82bhCwsOkHNGKKY=";
+          stripRoot = false;
+        };
+      };
+    in
+    sources.${pkgs.stdenv.hostPlatform.system}
+      or (throw "Unsupported system for vscode-ripgrep: ${pkgs.stdenv.hostPlatform.system}");
 in
 pkgs.stdenv.mkDerivation rec {
   pname = "bobshell";
@@ -61,7 +160,17 @@ pkgs.stdenv.mkDerivation rec {
     # Create node_modules directory and install the dependencies
     mkdir -p node_modules/@lydell
     cp -r ${nodePty}/package node_modules/@lydell/node-pty
-    cp -r ${nodePtyLinuxX64}/package node_modules/@lydell/node-pty-linux-x64
+    cp -r ${nodePtyPlatform.src}/package node_modules/@lydell/${nodePtyPlatform.pkgName}
+
+    mkdir -p node_modules/@officecli/officecli/vendor
+    cp -r ${officecli}/package/* node_modules/@officecli/officecli/
+    cp ${officecliBinary} node_modules/@officecli/officecli/vendor/officecli
+    chmod +x node_modules/@officecli/officecli/vendor/officecli
+
+    mkdir -p node_modules/@vscode/ripgrep/bin
+    cp -r ${vscodeRipgrep}/package/* node_modules/@vscode/ripgrep/
+    cp ${vscodeRipgrepBinary}/rg node_modules/@vscode/ripgrep/bin/rg
+    chmod +x node_modules/@vscode/ripgrep/bin/rg
 
     runHook postBuild
   '';
